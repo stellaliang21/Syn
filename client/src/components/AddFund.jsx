4a84a071ccd98cddd3d7c4312ae100f27f6a14bf
Stella Liang <stellaliang@Stellas-MacBook-Pro.local>
2019-08-20 12:01:51 -0700

    Add css and error handling

--- client/src/components/AddFund.jsx
+++ client/src/components/AddFund.jsx
@@ -2,6 +2,16 @@ import React from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
 
+const Info = styled.div`
+  border-style: solid;
+  border-width: 3px;
+  border-color: #4CAF50;
+  width: 700px;
+  height: 200px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+
 const InputForm = styled.form`
   border-style: solid;
   border-width: 3px;
@@ -14,7 +24,7 @@ const InputForm = styled.form`
   height: 250px;
   display: flex;
   flex-direction: column;
-`
+`;
 
 const Inputs = styled.input`
   width: 350px;
@@ -24,7 +34,7 @@ const Inputs = styled.input`
   display: inline-block;
   border: 1px solid #ccc;
   box-sizing: border-box;
-`
+`;
 
 const Submit = styled.input`
   background-color: #4CAF50; 
@@ -35,7 +45,18 @@ const Submit = styled.input`
   margin-left: auto;
   margin-right: auto;
   font-size: 16px;
-`
+`;
+
+const Button = styled.button`
+  background-color: #4CAF50; 
+  border: none;
+  color: white;
+  width: 180px;
+  height: 25px;
+  margin-left: auto;
+  margin-right: auto;
+  font-size: 16px;
+`;
 
 class AddFund extends React.Component{
   constructor(props) {
@@ -45,11 +66,12 @@ class AddFund extends React.Component{
       total_value: 0,
       user_id: '',
       node_id: '',
-
+      sucessfully_added: false
     };
     this.handleChange = this.handleChange.bind(this);
     this.addFund  = this.addFund.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.addMoreFund = this.addMoreFund.bind(this);
   }
 
   handleChange(e) {
@@ -67,43 +89,107 @@ class AddFund extends React.Component{
   }
 
   addFund(value) {
-    const { amount, total_value, user_id, node_id } = this.state;
+    const { amount, total_value, user_id, node_id, sucessfully_added } = this.state;
     axios.post (`/api/addfund/${user_id}`, {
       node_id: node_id,
       amount: amount
     })
     .then(response => {
       this.setState({
-        total_value: total_value + parseInt(value)
+        total_value: total_value + parseInt(value),
+        sucessfully_added: !sucessfully_added,
+        user_id: '',
+        node_id: '',
+        amount: 0
       });
       console.log(response);
     })
     .catch(error => {
       console.log(error);
+      this.setState({
+        sucessfully_added: 'error'
+      });
     });
   }
 
+  addMoreFund() {
+    const { sucessfully_added } = this.state;
+
+    this.setState({
+      sucessfully_added: !sucessfully_added
+    })
+  }
+
   render() {
+    const { sucessfully_added, total_value } = this.state;
+
+    let addFund;
+
+    if (sucessfully_added === false) {
+      addFund = 
+        <div>
+          <h1>Add Fund</h1>
+          <InputForm onSubmit={e => this.handleSubmit(e)}>
+          <label>
+                User ID:
+                <Inputs 
+                  type="text" 
+                  name="user_id" 
+                  onChange={this.handleChange} 
+                  placeholder={'User ID'} 
+                />
+            </label>
+            <label>
+                IB Account ID:
+                <Inputs 
+                  type="text" 
+                  name="node_id" 
+                  onChange={this.handleChange} 
+                  placeholder={'IB Account ID'} 
+                />
+            </label>
+            <label>
+                Amount:
+                <Inputs 
+                  type="text" 
+                  name="amount" 
+                  onChange={this.handleChange} 
+                  placeholder={'Amount'} 
+                />
+            </label>
+            <Submit type="submit" value="Submit" />
+          </InputForm> `
+        </div>
+    } else if (sucessfully_added === true) {
+      addFund =
+      <Info>
+        <h1>
+          Account Information
+        </h1>
+        <div>
+          Current Balance: ${total_value}
+        </div>
+        <Button onClick={this.addMoreFund}>
+          Add more funds
+        </Button>
+      </Info>
+    } else if (sucessfully_added === 'error') {
+      addFund =
+        <Info>
+          <h2>
+            Error, please try again later
+          </h2>
+          <Button onClick={this.addMoreFund}>
+            Add more funds
+          </Button>
+        </Info>
+    }
+
     return (
       <div>
-      <h1>Add Fund</h1>
-      <InputForm onSubmit={e => this.handleSubmit(e)}>
-      <label>
-            User ID:
-            <Inputs type="text" name="user_id" onChange={this.handleChange} placeholder={'User ID'} />
-        </label>
-        <label>
-            IB Account ID:
-            <Inputs type="text" name="node_id" onChange={this.handleChange} placeholder={'IB Account ID'} />
-        </label>
-        <label>
-            Amount:
-            <Inputs type="text" name="amount" onChange={this.handleChange} placeholder={'Amount'} />
-        </label>
-        <Submit type="submit" value="Submit" />
-      </InputForm> 
-      {this.state.total_value}
-    </div>
+        {addFund}
+      </div>
+      
     )
   }
 }
